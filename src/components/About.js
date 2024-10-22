3212bb2b2610544febd5563e9a3298c709bc6bdb
Zakaria Elharmouzi <z.elharmouzi@aui.ma>
2024-10-22 23:58:18 +0100

    added about us

--- src/components/About.js
+++ src/components/About.js
@@ -8,11 +8,7 @@ const About = () => {
 			<div className=" flex flex-col justify-center items-center space-y-5 lg:w-1/3 ">
 				<h2 className=" text-[#0E2368] font-bold text-center text-3xl ">About Us</h2>
 				<span className="text-[#444957] text-sm text-center lg:text-left ">
-					Lorem Ipsum is simply dummy text of the printing and
-					typesetting industry. Lorem Ipsum has been the industry's
-					standard dummy text ever since the 1500s, when an unknown
-					printer took a galley of type and scrambled it to make a
-					type specimen book. t has survived not only five centuries.
+				Sur notre blog culinaire, Ilyas Remmal, Mohamed Abbas, Nisrine Sadik et, Zakaria Elharmouzi, sommes passionnés par le partage de notre amour pour la bonne cuisine, saine et abordable. Nous croyons que bien manger ne doit pas être compliqué ni coûteux. À travers nos expériences à l’Université Al Akhawayn, nous partageons des astuces, des recettes et des histoires personnelles pour le prouver. Notre objectif est d'inspirer les autres à faire des choix nutritifs, à explorer de nouvelles saveurs et à prendre plaisir à cuisiner, que vous soyez étudiant ou simplement à la recherche de repas intelligents et délicieux.
 				</span>
 				<button className=" max-w-fit bg-[#E23744] text-white text-sm rounded-full p-2 px-5 ">En savoir plus</button>
 			</div>
