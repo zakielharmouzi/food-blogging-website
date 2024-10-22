ebc2a2c54182fb56956cdac61226c1c3bc6f41b7
Zakaria Elharmouzi <z.elharmouzi@aui.ma>
2024-10-22 21:49:06 +0100

    GPT COOKED LETSGOOO

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,4 +1,3 @@
-// Articles.js
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -7,46 +6,55 @@ const firstArticles = [
 	{
 		name: "KADS Plan",
 		url: "images/kadsplan.jpg", // Updated image URL
-		text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley....",
+		text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s....",
+		category: "Planning",
 	},
 	{
 		name: "”food prices” à Ifrane",
 		url: "images/increasing.png", // Updated image URL
-		text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley....",
+		text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry....",
+		category: "Economy",
 	},
 	{
 		name: "Mon Cuisine Étudiante !",
 		url: "images/mashups.jpeg", // Updated image URL
-		text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley....",
+		text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry....",
+		category: "Food",
 	},
 	{
 		name: "Delicious Pasta Salad",
-		url: "images/tajine.png", // Updated image URL
-		text: "A refreshing and easy pasta salad recipe for summer aaaaaaaaaaaaaaaaaaa Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.....",
-
-	}, // New fourth card
+		url: "images/soysauce.jpg", // Updated image URL
+		text: "A refreshing and easy pasta salad recipe for summer....",
+		category: "Recipes",
+	},
 ];
 
 const secondArticles = [
 	{
 		name: "How to Grill Corn",
 		url: "images/tajine.png", // Updated image URL
-		text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley....",
+		text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry....",
+		category: "Recipes",
 	},
 	{
 		name: "Crunchwrap Supreme",
 		url: "images/tajine.png", // Updated image URL
-		text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley....",
+		text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry....",
+		category: "Recipes",
 	},
 	{
 		name: "Broccoli Cheese Soup",
 		url: "images/tajine.png", // Updated image URL
-		text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley....",
+		text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry....",
+		category: "Recipes",
 	},
 ];
 
+const categories = ["All", "Planning", "Economy", "Food", "Recipes"]; // Category list
+
 const Articles = () => {
 	const [counter, setCounter] = useState(1);
+	const [selectedCategory, setSelectedCategory] = useState("All"); // State to track selected category
 	const navigate = useNavigate();
 
 	let cardsList;
@@ -55,6 +63,11 @@ const Articles = () => {
 	} else {
 		cardsList = firstArticles;
 	}
+
+	// Filter articles based on selected category
+	const filteredArticles = selectedCategory === "All"
+		? cardsList
+		: cardsList.filter((article) => article.category === selectedCategory);
 
 	const handleNext = () => {
 		setCounter(2);
@@ -67,8 +80,12 @@ const Articles = () => {
 	};
 
 	const handleReadMore = (name) => {
-		const formattedName = name.replace(/\s+/g, '-').toLowerCase(); // Format name for URL
+		const formattedName = name.replace(/\s+/g, '-').toLowerCase();
 		navigate(`/articles/${formattedName}`);
+	};
+
+	const handleCategoryChange = (category) => {
+		setSelectedCategory(category);
 	};
 
 	return (
@@ -78,42 +95,67 @@ const Articles = () => {
 				Nos Derniers Articles
 			</div>
 
-			{/* cards */}
+			{/* Category Filter */}
+			<div className="flex justify-center mb-6">
+				{categories.map((category) => (
+					<button
+						key={category}
+						onClick={() => handleCategoryChange(category)}
+						className={`border px-4 py-2 mx-2 rounded-lg ${
+							selectedCategory === category
+								? "bg-blue-500 text-white"
+								: "bg-gray-200"
+						}`}>
+						{category}
+					</button>
+				))}
+			</div>
+
+			{/* Cards */}
 			<div className="flex flex-col space-y-7 items-center space-x-1.5 lg:items-baseline lg:flex-row justify-between">
-				{cardsList.map((item) => (
-					 <div key={item.name}  
-					 className="flex flex-col border w-80 lg:w-[22rem] border-slate-300 bg-slate-50 rounded-3xl p-4"  
-					 >  
-					 <img alt={item?.name} src={item.url} className="my-2 object-cover h-48" /> {/* Set a fixed height for images */}  
-					 <div className="flex flex-col flex-grow justify-between"> {/* Add flex-grow here */}  
-					 <div className="flex flex-col items-center lg:items-start space-y-4">  
-					 <span className="text-[#0E2368] text-center text-[1.35rem] lg:text-right font-bold lg:text-[1.5rem]">  
-					 {item?.name}  
-					 </span>  
-					 <span className="text-sm lg:text-xs line-clamp-5 lg:line-clamp-3">  
-					 {item?.text}  
-					 </span>  
-					 </div>  
-					 <button onClick={() => handleReadMore(item.name)}  
-					 className="border border-black p-2 px-5 text-sm mt-12 rounded-full self-end"  
-					 >  
-					 Read More </button>  
-					 </div>  
-					 </div>  
+				{filteredArticles.map((item) => (
+					<div
+						key={item.name}
+						className="flex flex-col border w-80 lg:w-[22rem] border-slate-300 bg-slate-50 rounded-3xl p-4"
+					>
+						<img
+							alt={item?.name}
+							src={item.url}
+							className="my-2 object-cover h-48"
+						/>
+						<div className="flex flex-col flex-grow justify-between">
+							<div className="flex flex-col items-center lg:items-start space-y-4">
+								<span className="text-[#0E2368] text-center text-[1.35rem] lg:text-right font-bold lg:text-[1.5rem]">
+									{item?.name}
+								</span>
+								<span className="text-sm lg:text-xs line-clamp-5 lg:line-clamp-3">
+									{item?.text}
+								</span>
+							</div>
+							<button
+								onClick={() => handleReadMore(item.name)}
+								className="border border-black p-2 px-5 text-sm mt-12 rounded-full self-end"
+							>
+								Read More
+							</button>
+						</div>
+					</div>
 				))}
 			</div>
 
 			{/* Pagination buttons */}
-			<div className=" flex justify-center items-center space-x-4 text-sm py-10 ">
+			<div className="flex justify-center items-center space-x-4 text-sm py-10">
 				<button
-					onClick={() => setCounter(1)}
-					className=" w-6 pb-1 px-1 rounded-md border border-slate-700 visited:bg-slate-200 ">
+					onClick={() => handlePrev()}
+					className="w-6 pb-1 px-1 rounded-md border border-slate-700 visited:bg-slate-200"
+				>
 					&lt;
 				</button>
-				<span className=" text-xl">{counter}/2</span>
+				<span className="text-xl">{counter}/2</span>
 				<button
-					onClick={() => setCounter(2)}
-					className=" w-6 pb-1 px-1 rounded-md border border-slate-700 visited:bg-slate-200 ">
+					onClick={() => handleNext()}
+					className="w-6 pb-1 px-1 rounded-md border border-slate-700 visited:bg-slate-200"
+				>
 					&gt;
 				</button>
 			</div>
