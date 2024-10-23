248b24dd92dd1ba308190f557f2e01d2b8177683
Zakaria Elharmouzi <z.elharmouzi@aui.ma>
2024-10-23 01:09:30 +0100

    nice

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const About = () => {
 			<div className=" flex flex-col justify-center items-center space-y-5 lg:w-1/3 ">
 				<h2 className=" text-[#0E2368] font-bold text-center text-3xl ">About Us</h2>
 				<span className="text-[#444957] text-sm text-center lg:text-left ">
-				Sur notre blog culinaire, Ilyas Remmal, Mohamed Abbas, Nisrine Sadik et, Zakaria Elharmouzi, sommes passionnés par le partage de notre amour pour la bonne cuisine, saine et abordable. Nous croyons que bien manger ne doit pas être compliqué ni coûteux. À travers nos expériences à l’Université Al Akhawayn, nous partageons des astuces, des recettes et des histoires personnelles pour le prouver. Notre objectif est d'inspirer les autres à faire des choix nutritifs, à explorer de nouvelles saveurs et à prendre plaisir à cuisiner, que vous soyez étudiant ou simplement à la recherche de repas intelligents et délicieux.
+				Bienvenue sur notre blog culinaire, créé par Ilyas Remmal, Mohamed Abbas, Nisrine Sadik et Zakaria Elharmouzi. Nous partageons notre passion pour une cuisine saine, savoureuse et accessible à tous. Nous croyons fermement que bien manger ne doit pas être compliqué ni coûteux. À travers nos aventures à l’Université Al Akhawayn, nous dévoilons astuces, recettes et anecdotes personnelles pour démontrer que la nutrition peut être à la fois simple et agréable. Notre mission est d’encourager des choix alimentaires équilibrés, d’explorer de nouvelles saveurs et de redécouvrir le plaisir de cuisiner. Que vous soyez étudiant ou simplement en quête de repas astucieux et délicieux, rejoignez-nous pour un voyage culinaire enrichissant!
 				</span>
 				<button className=" max-w-fit bg-[#E23744] text-white text-sm rounded-full p-2 px-5 ">En savoir plus</button>
 			</div>
